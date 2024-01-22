6871e26bd1ffd86c8272f84cd5c83658471c4c05
mahdiiN <122620052+mahdiiN@users.noreply.github.com>
2024-01-22 22:03:51 +0100

    Add files via upload

---
+++ ai.dart
@@ -0,0 +1,220 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Fruit Recognizer",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isLoading = true;
+  File? _image;
+  List? _output;
+  String? res;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadTfliteModel();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    closeTflite();
+  }
+
+  void loadTfliteModel() async {
+    res = await Tflite.loadModel(
+      model: 'assets/model.tflite',
+      labels: 'assets/labels.txt',
+      numThreads: 1,
+      isAsset: true,
+      useGpuDelegate: false,
+    );
+  }
+
+  void closeTflite() async {
+    await Tflite.close();
+  }
+
+  void classifyImage(File? image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image!.path,
+      numResults: 36,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+
+    setState(() {
+      _output = output;
+      _isLoading = false;
+    });
+  }
+
+  void pickImage() async {
+    final image = await picker.pickImage(
+      source: ImageSource.camera,
+      preferredCameraDevice: CameraDevice.rear,
+    );
+
+    if (image == null) {
+      return null;
+    }
+    setState(() {
+      _image = File(image.path);
+    });
+    classifyImage(_image);
+  }
+
+  pickGalleryImage() async {
+    final image = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (image == null) {
+      return null;
+    }
+
+    setState(() {
+      _image = File(image.path);
+    });
+
+    classifyImage(_image);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Food Recognition",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            color: Colors.white, // Change the color to your desired color
+          ),
+        ),
+        centerTitle: true, // Center the title horizontally
+        backgroundColor: Color(0xFF34416D),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (_isLoading == true)
+            Container()
+          else
+            Container(
+              height: 250,
+              width: 250,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.file(
+                  _image!,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          const Divider(
+            height: 25,
+            thickness: 1,
+          ),
+          _output != null
+              ? Text(
+                  "I see ${_output![0]['label']} on the scale ",
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              : Container(),
+          const Divider(
+            height: 25,
+            thickness: 1,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: pickImage,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.7,
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF34416D),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: const Text(
+                        "Open Camera",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+
+                        ),
+
+                      ),
+
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: pickGalleryImage,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.7,
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF34416D),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: const Text(
+                        "Choose From Gallery",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
